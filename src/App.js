cde3e761d6249e6ad7eb94227fba58a2f3b77c5c
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-04-19 10:36:21 +0530

    Render all position tooltip

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import PositionForm from "./components/PositionForm/PositionForm";
+import Tooltip from "./components/Tooltip/Tooltip";
 
 function App() {
   const [position, setPosition] = useState("bottom");
@@ -15,7 +16,9 @@ function App() {
       <h1>Custom Tooltip</h1>
       <PositionForm handlePositionInput={handlePositionInput} />
       <div className="hoverElementAddTooltipContainer">
-        <div className="hoverElement">Hover Here</div>
+        <div className="hoverElement">
+          Hover Here <Tooltip />
+        </div>
       </div>
     </div>
   );
--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -2,8 +2,13 @@ import styles from "./Tooltip.module.css";
 
 function Tooltip() {
   return (
-    <div>
-      <span>Hii</span>
+    <div className={styles.left}>
+      <span>Wonderful Day? Isn't it?</span>
+      <span>
+        Hover element positioning{" "}
+        <span className={styles.position}>bottom</span>
+      </span>
+      <div></div>
     </div>
   );
 }
